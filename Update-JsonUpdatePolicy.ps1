337d8865b4f48f86c520ef045026b16d9845dfd2
Chris Stone <chris@stoneyforest.net>
2025-09-09 14:26:32 -0400

    20250909-02

--- Update-JsonUpdatePolicy.ps1
+++ Update-JsonUpdatePolicy.ps1
@@ -1,11 +1,11 @@
 <#
 .NOTES
 	Author:			Chris Stone <chris.stone@nuwavepartners.com>
-	Date-Modified:	2025-06-11 11:44:08
+	Date-Modified:	2025-09-09 14:10:55
 #>
 #Requires -Version 7
 
-################################## FUNCTIONS ##################################
+################################## FUNCTIONS ###################################
 
 function Get-WUCSearch {
 	Param (
@@ -42,91 +42,117 @@ function Get-WUCDownload {
 
 Write-Output ('Script Started ').PadRight(80, '-')
 
-$WUs = @()
-
-# Windows 10.x
-$ModernOSs = @(
-	@{WUName = "Windows 10 Version 1507"; Caption = "Microsoft Windows 10"; Version = "10.0.10240" },
-	@{WUName = "Windows 10 Version 1511"; Caption = "Microsoft Windows 10"; Version = "10.0.10586" },
-	@{WUName = "Windows 10 Version 1607"; Caption = "Microsoft Windows 10"; Version = "10.0.14393" },
-	@{WUName = "Windows 10 Version 1703"; Caption = "Microsoft Windows 10"; Version = "10.0.15063" },
-	@{WUName = "Windows 10 Version 1709"; Caption = "Microsoft Windows 10"; Version = "10.0.16299" },
-	@{WUName = "Windows 10 Version 1803"; Caption = "Microsoft Windows 10"; Version = "10.0.17134" },
-	@{WUName = "Windows 10 Version 1809"; Caption = "Microsoft Windows 10"; Version = "10.0.17763" },
-	@{WUName = "Windows 10 Version 1903"; Caption = "Microsoft Windows 10"; Version = "10.0.18362" },
-	@{WUName = "Windows 10 Version 1909"; Caption = "Microsoft Windows 10"; Version = "10.0.18363" },
-	@{WUName = "Windows 10 Version 20H1"; Caption = "Microsoft Windows 10"; Version = "10.0.19041" },
-	@{WUName = "Windows 10 Version 20H2"; Caption = "Microsoft Windows 10"; Version = "10.0.19042" },
-	@{WUName = "Windows 10 Version 21H1"; Caption = "Microsoft Windows 10"; Version = "10.0.19043" },
-	@{WUName = "Windows 10 Version 21H2"; Caption = "Microsoft Windows 10"; Version = "10.0.19044" },
-	@{WUName = "Windows 10 Version 22H2"; Caption = "Microsoft Windows 10"; Version = "10.0.19045" },
-	@{WUName = "Windows 11"; Caption = "Microsoft Windows 11"; Version = "10.0.22000" },
-	@{WUName = "Windows 11 Version 22H2"; Caption = "Microsoft Windows 11"; Version = "10.0.22621" },
-	@{WUName = "Windows 11 Version 23H2"; Caption = "Microsoft Windows 11"; Version = "10.0.22631" },
-	@{WUName = "Windows 11 Version 24H2"; Caption = "Microsoft Windows 11"; Version = "10.0.26100" },
-	@{WUName = "Windows Server 2016"; Caption = "Microsoft Windows Server 2016"; Version = "10.0.14393" },
-	@{WUName = "Windows Server 2019"; Caption = "Microsoft Windows Server 2019"; Version = "10.0.17763" },
-	@{WUName = "Microsoft server operating system version 21H2"; Caption = "Microsoft Windows Server 2022"; Version = "10.0.20348" }
-	#	@{WUName = "Microsoft server operating system, version 22H2"; Caption = "Microsoft Windows Server 2022"; Version = "10.0.20348" }
-	@{WUName = "Microsoft server operating system version 23H2"; Caption = "Microsoft Windows Server 2022"; Version = "10.0.25398" }
-	@{WUName = "Microsoft server operating system version 24H2"; Caption = "Microsoft Windows Server 2025"; Version = "10.0.26100"}
-)
-
-Foreach ($OS in $ModernOSs) {
-	Write-Output ("Finding updates for {0}" -f $OS.WUName)
-	[System.Collections.Generic.List[Hashtable]]$SearchUpdates = @(
-			(Get-WUCSearch ('"Servicing Stack Update for {0} for x64-based Systems" -Preview' -f $OS.WUName) | Select-Object -First 1),
-			(Get-WUCSearch ('"Cumulative Update for {0} for x64-based Systems" -Dynamic' -f $OS.WUName) | Select-Object -First 1),
-			(Get-WUCSearch ('"Microsoft .NET Framework" 4.8 for "{0}"' -f $OS.WUName) | Select-Object -First 1),
-			(Get-WUCSearch ('"Cumulative Update for .NET Framework {0} for x64"' -f $OS.WUName) | Select-Object -First 1)
-	)
-	$SearchUpdates.RemoveAll({ $args[0] -eq $null }) | Out-Null
-	$WUs += @{
-		OS      = @{
-			Caption = $OS.Caption;
-			Version = $OS.Version
-		}
-		Updates = $SearchUpdates.ToArray()
-	}
+# Load existing policy file if it exists
+$ExistingPolicy = $null
+If (Test-Path -Path ".\Windows-UpdatePolicy.json") {
+    Write-Output "Loading existing policy file"
+    $ExistingPolicy = Get-Content -Path ".\Windows-UpdatePolicy.json" | ConvertFrom-Json
 }
 
-# Windows 6.x
-$Win63OSs = @(
-	@{WUName = "Windows Server 2012 R2"; Caption = "Microsoft Windows Server 2012 R2"; Version = "6.3.9600" },
-	@{WUName = "Windows 8.1"; Caption = "Microsoft Windows 8.1"; Version = "6.3.9600" },
-	@{WUName = "Windows Server 2012"; Caption = "Microsoft Windows Server 2012"; Version = "6.2.9200" },
-	@{WUName = "Windows 8"; Caption = "Microsoft Windows 8"; Version = "6.2.9200" },
-	@{WUName = "Windows Server 2008 R2"; Caption = "Microsoft Windows Server 2008 R2"; Version = "6.1.7601" },
-	@{WUName = "Windows 7"; Caption = "Microsoft Windows 7"; Version = "6.1.7601" }
-)
-
-Foreach ($OS in $Win63OSs) {
-	Write-Output ("Finding updates for {0}" -f $OS.WUName)
-	[System.Collections.Generic.List[Hashtable]]$SearchUpdates = @(
-		(Get-WUCSearch ('"Servicing Stack Update for {0} for x64-based Systems" -Preview' -f $OS.WUName) | Select-Object -First 1),
-		(Get-WUCSearch ('"Security Monthly Quality Rollup for {0} for x64-based Systems" -Dynamic' -f $OS.WUName) | Select-Object -First 1),
-		(Get-WUCSearch ('"Microsoft .NET Framework" 4.8 for "{0}"' -f $OS.WUName) | Select-Object -First 1),
-		(Get-WUCSearch ('"Cumulative Update for .NET Framework {0} for x64"' -f $OS.WUName) | Select-Object -First 1)
-		(Get-WUCSearch ('{0} "KB3191564"' -f $OS.WUName) | Select-Object -First 1)
-		(Get-WUCSearch ('{0} "KB3191565"' -f $OS.WUName) | Select-Object -First 1)
-	)
-	$SearchUpdates.RemoveAll({ $args[0] -eq $null }) | Out-Null
-	$WUs += @{
-		OS      = @{
-			Caption = $OS.Caption;
-			Version = $OS.Version
-		}
-		Updates = $SearchUpdates.ToArray()
-	}
-}
+# Load OS Mapping Data from external JSON file
+$OSs = Get-Content -Path ".\Windows-Mapping.json" | ConvertFrom-Json
 
 # EoL Information
-
 $EoLURI = 'https://endoflife.date/api/windows.json'
 $EoL = (New-Object System.Net.WebClient).DownloadString($EoLURI) | ConvertFrom-Json
 
-# Generate
+# Define Update Search Specifications using embedded JSON
+$WUSpec = @'
+[
+    {
+        "VersionSpec": ">=10.0.0",
+        "QueryString": "'\"Servicing Stack Update for {0} for x64-based Systems\" -Preview'"
+    },
+    {
+        "VersionSpec": ">=10.0.0",
+        "QueryString": "'\"Cumulative Update for {0} for x64-based Systems\" -Dynamic'"
+    },
+    {
+        "VersionSpec": "<10.0.0",
+        "QueryString": "'\"Servicing Stack Update for {0} for x64-based Systems\" -Preview'"
+    },
+    {
+        "VersionSpec": "<10.0.0",
+        "QueryString": "'\"Security Monthly Quality Rollup for {0} for x64-based Systems\" -Dynamic'"
+    },
+    {
+        "VersionSpec": "*",
+        "QueryString": "'\"Microsoft .NET Framework\" 4.8 for \"{0}\"'"
+    },
+    {
+        "VersionSpec": "*",
+        "QueryString": "'\"Cumulative Update for .NET Framework {0} for x64\"'"
+    },
+    {
+        "VersionSpec": "<=6.3.9600",
+        "QueryString": "'{0} \"KB3191564\"'"
+    },
+    {
+        "VersionSpec": "==6.2.9200",
+        "QueryString": "'{0} \"KB3191565\"'"
+    }
+]
+'@ | ConvertFrom-Json
 
+$WUs = @()
+
+# Single loop to process all operating systems
+Foreach ($OS in $OSs) {
+
+	# Check End of Life status before searching for updates
+	$EoLInfo = $EoL | Where-Object { $_.latest -eq $OS.Version } | Sort-Object { [datetime]$_.eol } -Descending | Select-Object -First 1
+	If (($null -ne $EoLInfo) -and ([datetime]$EoLInfo.eol -lt (Get-Date).AddDays(-30))) {
+		Write-Output ("`t{0} past End of Life ({1}). Copying from existing policy." -f $OS.WUName, ([datetime]$EoLInfo.eol).ToString('yyyy-MM-dd'))
+
+        $ExistingOSUpdates = $null
+        if ($null -ne $ExistingPolicy) {
+            $ExistingOSUpdates = ($ExistingPolicy.WindowsUpdate | Where-Object { $_.OS.Version -eq $OS.Version }).Updates
+        }
+
+		$WUs += @{
+			OS      = @{
+				Caption = $OS.Caption;
+				Version = $OS.Version
+			}
+			Updates = if ($null -ne $ExistingOSUpdates) { $ExistingOSUpdates } else { @() }
+		}
+		Continue
+	}
+
+	Write-Output ("Finding updates for {0}" -f $OS.WUName)
+
+	[System.Collections.Generic.List[Hashtable]]$SearchUpdates = @()
+	$OSVersion = [version]$OS.Version
+
+	# Find applicable updates based on version specification
+	Foreach ($Spec in $WUSpec) {
+		$VersionCheck = switch -Regex ($Spec.VersionSpec) {
+			'^\*'          { $true }
+			'^>=([0-9.]+)' { $OSVersion -ge [version]$Matches[1] }
+			'^>([0-9.]+)'  { $OSVersion -gt [version]$Matches[1] }
+			'^<=([0-9.]+)' { $OSVersion -le [version]$Matches[1] }
+			'^<([0-9.]+)'  { $OSVersion -lt [version]$Matches[1] }
+			'^==([0-9.]+)' { $OSVersion -eq [version]$Matches[1] }
+			default        { $false }
+		}
+
+		If ($VersionCheck) {
+			$Query = $Spec.QueryString -f $OS.WUName
+			$SearchUpdates.Add((Get-WUCSearch -Query $Query | Select-Object -First 1))
+		}
+	}
+
+	$SearchUpdates.RemoveAll({ $args[0] -eq $null }) | Out-Null
+	$WUs += @{
+		OS      = @{
+			Caption = $OS.Caption;
+			Version = $OS.Version
+		}
+		Updates = $SearchUpdates.ToArray()
+	}
+}
+
+# Generate the final policy file
 $Out = @{
 	_meta         = @{
 		Date_Modified = (((Get-Date).ToUniversalTime() | Get-Date -f 's') + 'Z')
@@ -137,4 +163,4 @@ $Out = @{
 
 $Out | ConvertTo-Json -Depth 9 -AsArray | Out-File -FilePath ".\Windows-UpdatePolicy.json"
 
-$M
+Write-Output ('Script Finished ').PadRight(80, '-')
